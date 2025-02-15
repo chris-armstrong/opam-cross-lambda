135f3ea648ec81c5fbebedb8075bb576ca3c67fd
Chris Armstrong <chris-armstrong@users.noreply.github.com>
2025-02-16 10:00:11 +1100

    Revert "fix zig paths on Cygwin/MinGW"
    This reverts commit 67c2699e0da3f69e6478fab57f1c7b6a0541dfb8.

--- packages/conf-zig-wrapper-cross-x86_64-al2023/conf-zig-wrapper-cross-x86_64-al2023.0/files/build.sh
+++ packages/conf-zig-wrapper-cross-x86_64-al2023/conf-zig-wrapper-cross-x86_64-al2023.0/files/build.sh
@@ -3,17 +3,4 @@
 CROSS_NAME=$1
 ZIG=$(which zig)
 
-
-case "$(uname -s)" in
-    Linux*)     machine=Linux;;
-    Darwin*)    machine=Mac;;
-    CYGWIN*)    machine=Cygwin;;
-    MINGW*)     machine=MinGW;;
-    MSYS_NT*)   machine=MSys;;
-    *)          machine="UNKNOWN:${unameOut}"
-esac
-
-( [ "$machine" = "Cygwin" ] || [ "$machine" = "MinGW" ] ) && \
-  ZIG=$(cygpath -m "$ZIG")
-
 echo "zig_path: \"${ZIG}\"" >> "conf-zig-wrapper-${CROSS_NAME}.config"
