67c2699e0da3f69e6478fab57f1c7b6a0541dfb8
Chris Armstrong <chris-armstrong@users.noreply.github.com>
2025-02-16 09:23:21 +1100

    fix zig paths on Cygwin/MinGW

--- packages/conf-zig-wrapper-cross-x86_64-al2023/conf-zig-wrapper-cross-x86_64-al2023.0/files/build.sh
+++ packages/conf-zig-wrapper-cross-x86_64-al2023/conf-zig-wrapper-cross-x86_64-al2023.0/files/build.sh
@@ -3,4 +3,17 @@
 CROSS_NAME=$1
 ZIG=$(which zig)
 
+
+case "$(uname -s)" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGW;;
+    MSYS_NT*)   machine=MSys;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+( [ "$machine" = "Cygwin" ] || [ "$machine" = "MinGW" ] ) && \
+  ZIG=$(cygpath -m "$ZIG")
+
 echo "zig_path: \"${ZIG}\"" >> "conf-zig-wrapper-${CROSS_NAME}.config"
